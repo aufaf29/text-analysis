a07e00e1255dd3f5636621de333eebe728a4b23b
aufaf29 <13518009@std.stei.itb.ac.id>
2022-08-08 23:44:39 +0700

    add prediction service scripts

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import dill
+
+import pandas as pd
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+
+dill._dill._reverse_typemap['ClassType'] = type
 
 class TweetRequest(BaseModel):
     text: str
@@ -8,6 +14,9 @@ class TweetRequest(BaseModel):
 class ClassifyResponse(BaseModel):
     label: str
     score: float
+    
+class ClassifyResponseLite(BaseModel):
+    label: str
     
 app = FastAPI()
 
@@ -28,22 +37,45 @@ app.add_middleware(
     description="Classify hate speech from single text"
 )
 def hate_speech_classifier(request: TweetRequest):
+    text = request.text
+    
+    file =  open('model/hatespeech.pkl', 'rb')
+    model = dill.load(file)
+    file.close()
+    
+    pred_proba = model.predict_proba(pd.DataFrame({'text': [text]}))[0]
+    
     res = dict()
-    res["label"] = "undefined"
-    res["score"] = 0.99921
+    if (pred_proba >= 0.5):
+        res["label"] = "non hatespeech"
+        res["score"] = int(pred_proba * 100)
+    else:      
+        res["label"] = "hatespeech"
+        res["score"] = 100 - int(pred_proba * 100)
+    
     return res
 
 @app.post(
     "/hate-speech-topic/predict", 
-    response_model=ClassifyResponse,
+    response_model=ClassifyResponseLite,
     tags=["Hate Speech"], 
     name="post_single_hate_speech_topic_classifier", 
     summary="Classify hate speech with topic from single text",
     description="Classify hate speech with topic from single text"
 )
 def hate_speech_topic_classifier(request: TweetRequest):
+    text = request.text
+    
+    file =  open('model/hatespeech_topic.pkl', 'rb')
+    model = dill.load(file)
+    file.close()
+    
+    pred = model.predict(pd.DataFrame({'text': [text]}))[0]
+    
+    classes = ['gender hatespeech', 'other hatespeech', 'physical hatespeech', 'race hatespeech', 'religion hatespeech']
+    
     res = dict()
-    res["label"] = "undefined"
-    res["score"] = 0.99921
+    res["label"] = classes[pred]
+    
     return res
 
